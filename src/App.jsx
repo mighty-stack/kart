faec44701aa2d5b65f85a1825b1dc03959f45231
Your Name <alabiolumide@gmail.com>
2025-08-19 23:19:20 -0700

    first react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Navbar />
+      <Banner />
+    </div>
+  )
+}
+
+export default App
