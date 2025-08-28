e4a948c7a4d8643dc9cfc8fc12aea56ea562561d
Your Name <alabiolumide@gmail.com>
2025-08-28 09:06:55 -0700

    narbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import About from './pages/About'
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
   return(
-    <> 
+    <>
+      <Navbar />
       <Routes>
-       <Route path="/" element={<Home/>}/>
-       <Route path="/about" element={<About/>}/>
-       <Route path="/Sigin" element={<Signin/>}/>
-       <Route path="/Signup" element={<Signup/>}/>
-       <Route path="*" element={<NotFound/>}/>
-     </Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/Sigin" element={<Signin/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,21 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider, useStore } from 'react-redux'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 
+
+export const store = configureStore({
+  reducer: {},
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <Provider store={store}>
         <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
